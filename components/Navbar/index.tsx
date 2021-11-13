220bdfb8b04b95ca41ed8ee1b301ceb5b3e6b775
Franklin <franklinokolie86@gmail.com>
2021-11-13 10:56:13 +0100

    Built and styled the Header component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,26 @@
 import { NextComponentType } from "next";
 import styles from "./navbar.module.scss";
+import Logo from '../../public/assets/images/logo.svg'
+import Image from "next/image";
 
 const Navbar: NextComponentType = () => {
   return (
-    <header>
-      <nav>
+    <header className={styles.header}>
+      <Image
+        src={Logo}
+        alt="Logo of Jobhut"
+        width={100}
+        // height={500} automatically provided
+        // blurDataURL="data:..." automatically provided
+        // placeholder="blur" // Optional blur-up while loading
+      />
+      <nav className={styles['header__navbar']}>
         <ul>
           <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
+          <li>About</li>
+          <li>Features</li>
+          <li>Screenshot</li>
+          <li>Blog</li>
           <li>Download</li>
         </ul>
       </nav>
