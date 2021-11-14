03141c1577913cce27f8bf87b04a620c9e3464d7
Franklin <franklinokolie86@gmail.com>
2021-11-14 13:04:09 +0100

    Made the team members card

---
+++ components/Team/Card/index.tsx
@@ -0,0 +1,47 @@
+import styles from './card.module.scss'
+import One from "../../../public/assets/images/luzzy.svg";
+import Facebook from "../../../public/assets/icons/facebook.svg";
+import Instagram from "../../../public/assets/icons/instagram.svg";
+import Twitter from "../../../public/assets/icons/twitter.svg";
+import Youtube from "../../../public/assets/icons/youtube.svg";
+import Image from "next/image";
+
+interface CardProps{
+ name: string,
+ role: string,
+ desc: string,
+ image: HTMLImageElement
+}
+
+const Card: React.FC<CardProps> = ({name, role, desc,image}) => {
+
+  return (
+    <div className={styles.card}>
+      <div className={styles["card__pics"]}>
+        <Image src={image} alt="A woman" />
+      </div>
+      <h4 className={styles["card__name"]}>{name}</h4>
+      <h6 className={styles["card__role"]}>{role}</h6>
+      <p className={styles["card__desc"]}>
+       {desc}
+      </p>
+
+      <div className={styles["card__social"]}>
+        <div>
+          <Image src={Facebook} alt="a facebook icon" />
+        </div>
+        <div>
+          <Image src={Instagram} alt="a instagram icon" />
+        </div>
+        <div>
+          <Image src={Twitter} alt="a twitter icon" />
+        </div>
+        <div>
+          <Image src={Youtube} alt="a youtube icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card
--- components/Team/index.tsx
+++ components/Team/index.tsx
@@ -1,13 +1,15 @@
 import styles from "./team.module.scss";
 import Container from "../Container";
-import One from '../../public/assets/images/luzzy.svg'
-import Two from '../../public/assets/images/henry.svg'
-import Three from '../../public/assets/images/raphael.svg'
-import Facebook from '../../public/assets/icons/facebook.svg'
-import Instagram from '../../public/assets/icons/instagram.svg'
-import Twitter from '../../public/assets/icons/twitter.svg'
-import Youtube from '../../public/assets/icons/youtube.svg'
-import Image from 'next/image'
+import One from "../../public/assets/images/luzzy.svg";
+import Two from "../../public/assets/images/henry.svg";
+import Three from "../../public/assets/images/raphael.svg";
+import Facebook from "../../public/assets/icons/facebook.svg";
+import Instagram from "../../public/assets/icons/instagram.svg";
+import Twitter from "../../public/assets/icons/twitter.svg";
+import Youtube from "../../public/assets/icons/youtube.svg";
+import Image from "next/image";
+
+import Card from "./Card";
 
 const Team: React.FC = () => {
   return (
@@ -24,90 +26,27 @@ const Team: React.FC = () => {
         </div>
 
         <div className={styles["team__cards"]}>
-          <div className={styles["team__cards-card"]}>
-            <div className={styles["team__cards__pics"]}>
-              <Image src={One} alt="A woman" />
-            </div>
-            <h4 className={styles["team__cards-card__name"]}>LUIZZY LUIZZY</h4>
-            <h6 className={styles["team__cards-card__role"]}>
-              PROJECT MANAGER
-            </h6>
-            <p className={styles["team__cards-card__desc"]}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
-              nunc ante velit vitae. Est tellus vitae.
-            </p>
-
-            <div className={styles["team__cards-card__social"]}>
-              <div>
-                <Image src={Facebook} alt="a facebook icon" />
-              </div>
-              <div>
-                <Image src={Instagram} alt="a instagram icon" />
-              </div>
-              <div>
-                <Image src={Twitter} alt="a twitter icon" />
-              </div>
-              <div>
-                <Image src={Youtube} alt="a youtube icon" />
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["team__cards-card"]}>
-            <div className={styles["team__cards__pics"]}>
-              <Image src={Two} alt="A Man smiling" />
-            </div>
-            <h4 className={styles["team__cards-card__name"]}>HENRY KAKA</h4>
-            <h6 className={styles["team__cards-card__role"]}>FOUNDER/CEO</h6>
-            <p className={styles["team__cards-card__desc"]}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
-              nunc ante velit vitae. Est tellus vitae.
-            </p>
-
-            <div className={styles["team__cards-card__social"]}>
-              <div>
-                <Image src={Facebook} alt="a facebook icon" />
-              </div>
-              <div>
-                <Image src={Instagram} alt="a instagram icon" />
-              </div>
-              <div>
-                <Image src={Twitter} alt="a twitter icon" />
-              </div>
-              <div>
-                <Image src={Youtube} alt="a youtube icon" />
-              </div>
-            </div>
-          </div>
-
-          <div className={styles["team__cards-card"]}>
-            <div className={styles["team__cards__pics"]}>
-              <Image src={Three} alt="A woman" />
-            </div>
-            <h4 className={styles["team__cards-card__name"]}>
-              RAPHAEL RAPHAEL
-            </h4>
-            <h6 className={styles["team__cards-card__role"]}>LEAD DEVELOPER</h6>
-            <p className={styles["team__cards-card__desc"]}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
-              nunc ante velit vitae. Est tellus vitae.
-            </p>
-
-            <div className={styles["team__cards-card__social"]}>
-              <div>
-                <Image src={Facebook} alt="a facebook icon" />
-              </div>
-              <div>
-                <Image src={Instagram} alt="a instagram icon" />
-              </div>
-              <div>
-                <Image src={Twitter} alt="a twitter icon" />
-              </div>
-              <div>
-                <Image src={Youtube} alt="a youtube icon" />
-              </div>
-            </div>
-          </div>
+          <Card
+            name="LUIZZY LUIZZY"
+            role=" PROJECT MANAGER"
+            desc=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+              nunc ante velit vitae. Est tellus vitae."
+            image={One}
+          />
+          <Card
+            name="HENRY KAKA"
+            role=" FOUNDER/CEO"
+            desc=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+              nunc ante velit vitae. Est tellus vitae."
+            image={Two}
+          />
+          <Card
+            name="RAPHAEL RAPHAEL"
+            role="LEAD DEVELOPER"
+            desc=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+              nunc ante velit vitae. Est tellus vitae."
+            image={Three}
+          />
         </div>
       </section>
     </Container>
