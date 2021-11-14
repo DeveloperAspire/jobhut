598fda10006024c3c133d981ca2ca92321408c3e
Franklin <franklinokolie86@gmail.com>
2021-11-14 13:14:34 +0100

    Added the necceassry props to the news card

--- components/Blog/Card/index.tsx
+++ components/Blog/Card/index.tsx
@@ -1,27 +1,28 @@
-import Snap from '../../../public/assets/images/snapchat.svg'
-import styles from './card.module.scss'
-import Image from 'next/image'
-const Card = () => {
+import Snap from "../../../public/assets/images/snapchat.svg";
+import styles from "./card.module.scss";
+import Image from "next/image";
 
-      return (
-        <div className={styles.card}>
-          <div className={styles["card__img"]}>
-            <Image src={Snap} alt="A blog cover photo showing snap chat" />
-          </div>
-          <div className={styles["card__content"]}>
-            <h6 className={styles["card__content-title"]}>
-              The Snap Pixel: How It Works and How to Install
-            </h6>
-            <p className={styles["card__content-desc"]}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
-              nunc ante velit vitae. Est tellus vitae, nullam lobortis enim.
-            </p>
-            <a className={styles["card__content-link"]} href="/blog">
-              Read More
-            </a>
-          </div>
-        </div>
-      );
+interface CardProps {
+  image: HTMLImageElement;
+  title: string;
+  link: string;
+  desc: string;
 }
+const Card: React.FC<CardProps> = ({ image, title, link, desc }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles["card__img"]}>
+        <Image src={image} alt="A blog cover photo showing snap chat" />
+      </div>
+      <div className={styles["card__content"]}>
+        <h6 className={styles["card__content-title"]}>{title}</h6>
+        <p className={styles["card__content-desc"]}>{desc}</p>
+        <a className={styles["card__content-link"]} href={link}>
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- components/Blog/index.tsx
+++ components/Blog/index.tsx
@@ -1,32 +1,47 @@
-import Card from "./Card/index"
-import Container from "../Container"
-import styles from './blog.module.scss'
+import Card from "./Card/index";
+import Container from "../Container";
+import styles from "./blog.module.scss";
+import Snap from "../../public/assets/images/snapchat.svg";
+import Global from "../../public/assets/images/global.svg";
+import Join from "../../public/assets/images/join.svg";
 
 const Blog = () => {
+  return (
+    <Container>
+      <section className={styles.section}>
+        <div className={styles["section__heading"]}>
+          <h4 className={styles["section__heading-title"]}>Our recent blog</h4>
+          <p className={styles["seection__heading-desc"]}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
+            nunc ante velit vitae. Est tellus vitae, nullam lobortis enim.
+            Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.
+            Ullamcorper risus tempor, ac nunc libero urna, feugiat.
+          </p>
+        </div>
 
-      return (
-        <Container>
-          <section className={styles.section}>
-            <div className={styles["section__heading"]}>
-              <h4 className={styles["section__heading-title"]}>
-                Our recent blog
-              </h4>
-              <p className={styles["seection__heading-desc"]}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra
-                nunc ante velit vitae. Est tellus vitae, nullam lobortis enim.
-                Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.
-                Ullamcorper risus tempor, ac nunc libero urna, feugiat.
-              </p>
-            </div>
+        <div className={styles["section__blog-cards"]}>
+          <Card
+            image={Snap}
+            title="The Snap Pixel: How It Works and How to Install"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim."
+            link="/blog"
+          />
+          <Card
+            image={Global}
+            title="Global Partner Solutions: A Partnership of Innovation"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim."
+            link="/blog"
+          />
+          <Card
+            image={Join}
+            title="2021: An opportunity for Snapchatters to start fresh"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim."
+            link="/blog"
+          />
+        </div>
+      </section>
+    </Container>
+  );
+};
 
-            <div className={styles["section__blog-cards"]}>
-              <Card />
-              <Card />
-              <Card />
-            </div>
-          </section>
-        </Container>
-      );
-}
-
-export default Blog
+export default Blog;
